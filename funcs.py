19c127bfdefecec8b903e3ef38d666f738f00510
Jack Risse <j.risse14@gmail.com>
2019-10-22 16:32:46 -0400

    added a function and hypothesis

---
+++ funcs.py
@@ -0,0 +1,32 @@
+import sqlite3
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def sql_to_df(db, query):
+
+    """
+    Parameters
+    -----------
+    db: string
+        sqlite database file path
+    query: string
+        sql query
+
+    Returns
+    -----------
+    df: DataFrame
+        dataframe created using the sql query
+    """
+
+    conn = sqlite3.connect(db)
+
+    df = pd.read_sql_query(query, conn)
+
+    conn.close()
+
+    df.head()
+
+    return df
